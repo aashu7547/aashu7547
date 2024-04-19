f56d19a2bf7db517c330f87374cef0d041845701
Aashutosh Kumar <159616195+aashu7547@users.noreply.github.com>
2024-04-20 01:45:29 +0530

    Add files via upload

---
+++ Loopsreading.java
@@ -0,0 +1,11 @@
+public class Loopsreading {
+    public static void main(String[] args) {
+        int  s= 100;
+        while (s<=100) {
+            
+        }
+            System.out.println(s);
+            s++;
+        }
+    }
+
