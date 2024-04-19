999e5555b2429b7425c1c57ea49e768f3fdaefc8
Aashutosh Kumar <159616195+aashu7547@users.noreply.github.com>
2024-04-20 01:46:43 +0530

    Add files via upload

---
+++ Set.java
@@ -0,0 +1,34 @@
+/**
+ * Set
+ */
+public class Set {
+
+    public static void main(String[] args) {
+      //  String name = "Aashutosh";
+//   System.out.println(name);
+     //   int value = name.length();
+     //System.out.println(value);
+//String bad = name.toLowerCase();
+ //System.out.println(bad);
+//String nad = name.toUpperCase();
+//System.out.println(nad);
+/*String car = "   Sachidanand           ";
+String name1 =car.trim();
+System.out.println(name1);*/
+/*String name1 = "Sachidanad";
+String map = name1.substring(1);
+System.out.println(map);*/
+/*String a = "Vishal";
+String d = a.replace('h','a');
+System.out.println(d);*/
+/*String mat = "Rohit";
+System.out.println(mat.startsWith("Ro"));*/
+/*  String name = "AashutoshKumar";
+        System.out.println(name.endsWith("ar"));*/
+        String nsame = "Sachidanand";
+       // System.out.println(nsame.charAt(5));
+           // System.out.println(nsame.indexOf("Sachida"));
+          //  System.out.println(nsame.equals("Sachidanand"));
+                System.out.println(nsame.equalsIgnoreCase("SACHIDANAND"));
+    }
+}
