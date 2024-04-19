bb7e69261376e0c2494be34f4a979ed5b8234d9d
Aashutosh Kumar <159616195+aashu7547@users.noreply.github.com>
2024-04-20 01:46:20 +0530

    Add files via upload

---
+++ Practics.java
@@ -0,0 +1,12 @@
+import java.awt.Button;
+public class Practics {
+    Practics(){
+        Button b =new Button("Click Me!!");
+        b.setBounds(30,100,80,30);
+        
+    }
+    public static void main(String[] args) {
+        Practics f =new Practics();
+        System.out.println(f);
+    }
+}
