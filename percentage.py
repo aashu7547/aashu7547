27d790ba61602f5bbed20da9c4ad914a342a79c3
Aashutosh Kumar <159616195+aashu7547@users.noreply.github.com>
2024-08-07 17:24:15 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,16 @@
+
+# # Find the  percentage of five subject
+# math = float(input("Enter your maths marks : "))
+# english = float(input("Enter your english marks : "))
+# phy = float(input("Enter your phy marks : "))
+# che = float(input("Enter your che marks : "))
+# hindi = float(input("Enter your hindi marks : "))
+# total = float(input("Enter your total marks : "))
+# percentage = ((math+english+phy+che+hindi)/total)*100
+# print(f"The percentage is {percentage:.2f}%")
+# a = [2, 3, 6, 5, 8]
+# b = [x * x for x in a]
+# print(b)
+# a = [25,69,78,500]
+# b = sum(x * x for x in a)
+# print(b)
